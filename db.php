e4083502ec60060229a6a02c7d2242e564bc9073
58160534 <58160534@go.buu.ac.th>
2017-03-22 21:18:32 +0700

    twetykl6

---
+++ db.php
@@ -0,0 +1,8 @@
+<?
+$dbhost = 'localhost';
+$dbuser = 'it58160534';
+$passwd = 'wangjackson';
+$dbname = 'it58160534';
+$conn = new mysqli("$dbhost","$dbuser","$passwd","$dbname");
+$conn->query('SET NAMES UTF8'); 
+?>
